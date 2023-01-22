3d96154b67d46088dca3853119dd36ba372dceba
Aleksandr Gribanov <aleksandr.gribanov@gmail.com>
2023-01-22 18:22:46 +0200

    Add Auth

--- server.js
+++ server.js
@@ -1,10 +1,31 @@
+const { v4 } = require('uuid');
+
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('db.json'); // <== Will be created later
+const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3200; // <== You can change the port
+const port = process.env.PORT || 3200;
 
 server.use(middlewares);
+server.use(jsonServer.bodyParser);
+server.post('/auth/login', (req, res) => {
+    const { login, password } = req.body;
+
+    if (login && password && login === password) {
+        res.json({ token: v4() });
+    } else {
+        res.status(401);
+        res.json({ error: 'login or password is invalid' });
+    }
+});
+
+server.use((req, res, next) => {
+    const token = req.headers.authorization;
+    if (!token) {
+        return res.status(401).send();
+    }
+    next();
+});
 server.use(router);
 
 server.listen(port);
